f4c394cb1f145383bd946b842154a5e1bb832146
leiliqajar22 <leiliqajar22@gmail.com>
2024-06-16 11:59:29 +1000

    Example fonts

--- src/components/Example.js
+++ src/components/Example.js
@@ -5,13 +5,13 @@ export default class Example extends Component {
   render() {
     return (
       <div className='exampleBox'>
-            <div className="example-logo logo">LOGO</div>
+            <div className="example-logo "><span>LOGO</span></div>
             <div className='example-text'>
                 <p className="upper-example-text">At the end of the day, our goal is simple: to
                     provide reliable, exceptional plumbing services
                     that exceed expectations</p>
                 <div className="lower-example-text">This dedication to customer satisfaction, along with our experience and technical expertise, is what
-                    sets us apart.
+                    sets us apart. <br />
                     Get started today with <a className='example-link' href='#0'>plumbing example</a> </div>
             </div>
       </div>
