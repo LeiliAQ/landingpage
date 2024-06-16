238fe23a15180d0a65f485aef6b01a4521985947
leiliqajar22 <leiliqajar22@gmail.com>
2024-06-16 10:18:26 +1000

    Onsitedeliver&Repairbanner minor changes(fonts margins..)

--- src/components/Others/Repairbanner.js
+++ src/components/Others/Repairbanner.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import './Repairbanner.css'
 import paymentImg from '../../images/payment-card.png'
+import rapiraLogo from '../../images/Asset 24.png'
 
 export default class Repairbanner extends Component {
   render() {
     return (
       <div className='repairbanner-container'>
         <div className="repair-box">
-            <div className="logo"><span className='repair-logo'>LOGO</span></div>
+            {/* <div className="repair-logo-container"><span className='repair-logo'>LOGO</span></div> */}
+            <div className="repair-logo-icon"><img className="logo-image-repair" src={rapiraLogo} alt="" /></div>
             <div className="repair-text">
                 <div className="repair-text__title">Ask about our payment plans</div>
                 <div className="repair-text-desc">Repair now, Pay Later</div>
                 <div className="repair-text-buttons">
-                    <a href="" className="repair-text-button btn">1300 000 000</a>
-                    <a href="" className="repair-text-button repair-text-button--snow btn">Get a free quote</a>
+                    <a href="#0" className="repair-text-button btn">1300 000 000</a>
+                    <a href="#0" className="repair-text-button repair-text-button--snow btn">Get a free quote</a>
                 </div>
             </div>
             <div className="repair-img">
@@ -26,21 +28,21 @@ export default class Repairbanner extends Component {
                 <div className="do__digit">01</div>
                 <div className="do__text">
                     <p className='do__text-title'>Do it now</p>
-                    <p>Big jobs or little jobs, repaired today.</p>
+                    <p className='do__text-desc'>Big jobs or little jobs, repaired today.</p>
                 </div>
             </div>
             <div className="inner-box">
             <div className="do__digit">02</div>
                 <div className="do__text">
                     <p className='do__text-title'>Pay later</p>
-                    <p>Pay it off in small, affordable installments.</p>
+                    <p className='do__text-desc'>Pay it off in small, affordable installments.</p>
                 </div>
             </div>
             <div className="inner-box">
             <div className="do__digit">03</div>
                 <div className="do__text">
                     <p className='do__text-title'>No interest ever</p>
-                    <p>Talk to us to find out more.</p>
+                    <p className='do__text-desc'>Talk to us to find out more.</p>
                 </div>
             </div>
         </div>
