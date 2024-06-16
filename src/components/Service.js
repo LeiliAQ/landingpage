36e482b55c7a45f2632c444f6f88c003c15ecb27
leiliqajar22 <leiliqajar22@gmail.com>
2024-06-16 10:58:24 +1000

    icons added to Service

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import './Service.css'
+import pipesIcon from '../images/Asset 29.png'
 
 export default class Service extends Component {
   render() {
     return (
       <div className='Service'>
             <div className="bar">
-                <div className="icon-containing"></div>
+                <div className="icon-containing">
+                    <img src={pipesIcon} alt="" />
+                </div>
                 <div className="bottom-blue-bar"><p className="hot-water">{this.props.text}</p></div>
             </div>
       </div>
