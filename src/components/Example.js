9d099b7da4eb6e4c84e12f0abd4f2d4e6676dd47
leiliqajar22 <leiliqajar22@gmail.com>
2024-06-17 13:49:36 +1000

    no message

--- src/components/Example.js
+++ src/components/Example.js
@@ -7,13 +7,16 @@ export default class Example extends Component {
       <div className='exampleBox'>
             <div className="example-logo "><span>LOGO</span></div>
             <div className='example-text'>
-                <p className="upper-example-text">At the end of the day, our goal is simple: to
+              <div>
+                
+              </div>
+                <div className="upper-example-text">At the end of the day, our goal is simple: to
                     provide reliable, exceptional plumbing services
-                    that exceed expectations</p>
+                    that exceed expectations</div>
                 <div className="lower-example-text">This dedication to customer satisfaction, along with our experience and technical expertise, is what
                     sets us apart. <br />
                     Get started today with <a className='example-link' href='#0'>plumbing example</a> </div>
-            </div>
+                </div>
       </div>
     )
   }
